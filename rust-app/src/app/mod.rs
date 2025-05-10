6c6ba88d7c6d272d9d4c9eeca35b054b1fc59f85
Liam Kosar <kosar.liam@gmail.com>
2025-05-10 00:51:27 -0400

    Feat: hella work

--- rust-app/src/app/mod.rs
+++ rust-app/src/app/mod.rs
@@ -3,21 +3,17 @@ use crate::our_redis::get_redis_client;
 use redis::Commands;
 
 pub fn run_app() {
-    if let Ok(number) = fetch_an_integer() {
-        println!("IM FUCKING APPPP The number is {}", number);
-    } else {
-        println!("IM FUCKING APPPP Oh no! Failure!");
-    }
+    println!("Rust App");
+    fetch_an_integer();
 }
 
-fn fetch_an_integer() -> redis::RedisResult<isize> {
+fn fetch_an_integer() {
     let mut con: redis::Connection = get_redis_client();
 
     // let _: () = con.set("my_key", 42)?;
     // con.get("my_key")'
     print!("MEOWWW");
-    let _: redis::RedisResult<isize> = con.rpush(QUEUE_NAME, 100);
-    let _: redis::RedisResult<isize> = con.rpush(QUEUE_NAME, 101);
-    let _: redis::RedisResult<isize> = con.rpush(QUEUE_NAME, 102);
-    con.rpush(QUEUE_NAME, 103)
+    for _ in 0..700 {
+        let _: redis::RedisResult<isize> = con.rpush(QUEUE_NAME, 42);
+    }
 }
